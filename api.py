b99ab4684d336f97227284066460161ad2e34c6f
Nick Rose <nicksrose72@gmail.com>
2022-08-01 10:50:41 -1000

    stand it uppp

--- api.py
+++ api.py
@@ -1,51 +1,54 @@
 from flask import Flask, request, jsonify
+import openai
+
 app = Flask(__name__)
+OPENAI_API_KEY = "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"
 
 
-@app.route('/getmsg/', methods=['GET'])
+
+def openai_complete(comment, prompt_start):
+    openai.api_key = OPENAI_API_KEY
+    prompt = prompt_start
+    prompt += 'Text: "{}"'.format(comment) + '\n\n'
+    prompt += "Label: "
+
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+    return response
+
+
+@app.route('/negativity_finder/', methods=['POST'])
 def respond():
-    # Retrieve the name from the url parameter /getmsg/?name=
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"Received: {name}")
+    text = request.form.get("text", None)
 
     response = {}
 
-    # Check if the user sent a name at all
-    if not name:
-        response["ERROR"] = "No name found. Please send a name."
-    # Check if the user entered a number
-    elif str(name).isdigit():
-        response["ERROR"] = "The name can't be numeric. Please send a string."
+    if not text:
+        response["ERROR"] = "No review text found. Please supply the text of the review in the request body".
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
+        openai_response = openai_complete(text, prompt_start="Find anything negative mentioned in this review:\n\n")
+        response_text = openai_response.get('choices')[0].text.strip("\n")
+        response['Review text'] = text
+        response['Sentiment'] = response_text
+
 
     # Return the response in json format
     return jsonify(response)
 
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome API!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "No name found. Please send a name."
-        })
 
 
 @app.route('/')
 def index():
     # A welcome message to test our server
-    return "<h1>Welcome to our medium-greeting-api!</h1>"
+    return "<h1>Welcome to the Review Sentiment Analyzer API</h1>"
 
 
 if __name__ == '__main__':
