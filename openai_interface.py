0ca9d24769f27185ced9b8ccbc968f643016240f
Nick Rose <nicksrose72@gmail.com>
2022-08-25 09:41:19 -0700

    humblebrag endpoints

--- openai_interface.py
+++ openai_interface.py
@@ -54,3 +54,18 @@ def openai_answer(question, text, extra_context="", temperature=0, max_tokens=10
         max_tokens=max_tokens,
         temperature=temperature
     )
+
+
+def openai_classify_humblebrag(text):
+    openai.api_key = OPENAI_API_KEY
+    prompt="Decide whether a LinkedIn post's sentiment is bragging or not bragging.\n\nPost: \"I gained 5M followers over the course of 2 months. Here's how:\"\nSentiment: Bragging\n\nPost: \"Humbled & excited to announce that I've just accepted an offer to join Google as a software engineer.\"\nSentiment: Bragging\n\nPost: \"Learned a lot from this livestream session on how to improve your site's checkout conversion\"\nSentiment: Not bragging\n\nPost: \"Now I'm out of school and solving problems most people choose not to address because it's simply human nature to take the path of least resistance. Are you ready for what I'm about to do? Because I am.\"\nSentiment: Bragging\n\n"
+    prompt += "Post: \"" + text + "\"\nSentiment: \n\n"
+    return openai.Completion.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0.5,
+        presence_penalty=0
+    )
