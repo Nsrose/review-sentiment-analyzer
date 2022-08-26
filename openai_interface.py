7f1f585930fe5c00d9a917b2d13c7ec698e51f0e
Nick Rose <nicksrose72@gmail.com>
2022-08-26 16:54:57 -0700

    back to davinci

--- openai_interface.py
+++ openai_interface.py
@@ -61,7 +61,7 @@ def openai_classify_humblebrag(text):
     prompt="Decide whether a LinkedIn post's sentiment is bragging or not bragging.\n\nPost: \"I gained 5M followers over the course of 2 months. Here's how:\"\nSentiment: Bragging\n\nPost: \"Humbled & excited to announce that I've just accepted an offer to join Google as a software engineer.\"\nSentiment: Bragging\n\nPost: \"Learned a lot from this livestream session on how to improve your site's checkout conversion\"\nSentiment: Not bragging\n\nPost: \"Now I'm out of school and solving problems most people choose not to address because it's simply human nature to take the path of least resistance. Are you ready for what I'm about to do? Because I am.\"\nSentiment: Bragging\n\n"
     prompt += "Post: \"" + text + "\"\nSentiment: \n"
     return openai.Completion.create(
-        model="text-curie-001",
+        model="text-davinci-002",
         prompt=prompt,
         temperature=0,
         max_tokens=60,
